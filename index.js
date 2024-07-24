a8075c854ff80508f579fcd469839412db055edf
rushikesherk <rushikesh.sasamkar@eronkan.com>
2024-07-24 22:45:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()   
+const port = process.env.PORT;
+
+app.get('/', (req,res)=>{
+    req.send("Hello world")
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send("You are in the twitter portal!")
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<h1>Login page</h1>')
+})
+
+app.listen(port,()=>{
+    console.log(`App listening port ${[port]}`)
+})
