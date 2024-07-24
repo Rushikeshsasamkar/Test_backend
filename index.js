4ca7d8ce5170446a7c3463fb676da99e62a6bac1
rushikesherk <rushikesh.sasamkar@eronkan.com>
2024-07-25 00:01:16 +0530

    modified

--- index.js
+++ index.js
@@ -3,9 +3,35 @@ require('dotenv').config()
 const express = require('express')
 const app = express()   
 const port = process.env.PORT;
+const packageData = {
+    
+        "name": "demodeploy1",
+        "version": "1.0.0",
+        "description": "basic app to deploy",
+        "main": "index.js",
+        "scripts": {
+          "test": "echo \"Error: no test specified\" && exit 1",
+          "start": "node index.js"
+        },
+        "keywords": [
+          "node"
+        ],
+        "author": "Rushikesh sasamkar",
+        "license": "ISC",
+        "dependencies": {
+          "dotenv": "^16.4.5",
+          "express": "^4.19.2"
+        }
+}
+      
+
 
 app.get('/', (req,res)=>{
     res.send("Hello world")
+})
+
+app.get('/github',(req,res)=>{
+    res.json(packageData);
 })
 
 app.get('/twitter',(req,res)=>{
